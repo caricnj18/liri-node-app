e087a7b7197c897415c67d80b3fbe64fd29b0651
carina cardona <carina.cardona89@gmail.com>
2018-04-25 17:00:55 -0400

    spotify and twitter working OMDB maybe

--- liri.js
+++ liri.js
@@ -1,1 +1,28 @@
 require("dotenv").config();
+
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var omdb = require('OMDB');
+var keys = require("./keys");
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+
+spotify
+  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+  .then(function(data) {
+    console.log(data); 
+  })
+  .catch(function(err) {
+    console.error('Error occurred: ' + err); 
+  });
+
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
